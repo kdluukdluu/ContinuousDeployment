a2b59c13b35b6b3c163d3e6d034fc67dd7c43a4b
Khai Luu <khai.luu@ktlconsult.com>
2017-12-10 15:58:18 -0500

    nc3

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -17,7 +17,6 @@ namespace ContinuousWeb.Controllers
 
         public IActionResult About()
         {
-            //throw new NotImplementedException();
             ViewData["Message"] = "Your application description page.";
             return View();
 
